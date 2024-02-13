0238b319dce155efe680f19c86e7c958e85a0b34
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-02-13 20:41:03 +0530

    Create Qn-03.cpp

---
+++ Qn-03.cpp
@@ -0,0 +1,13 @@
+//WAP in c++ to calculate sum of two numbers.
+
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int x , y ;
+    cout<<"Enter two numbers : " ;
+    cin>>x>>y ;
+
+    cout<<"Sum of "<<x<<" and "<<y<< " is "<<x+y ;
+    return 0 ;
+}
