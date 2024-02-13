6b4ecc1fd414ef4bbdc6cec85db593fe55fb3bb4
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-02-13 20:38:45 +0530

    Create Qn-02.cpp

---
+++ Qn-02.cpp
@@ -0,0 +1,10 @@
+//WAP in c++ to printf "Hello" on the 1st line and "MySirG" on the 2nd line using endl.
+
+#include<iostream>
+using namespace std ;
+
+int main ()
+{
+    cout<<"Hello"<<endl<<"MySirG";
+    return 0 ;
+}
