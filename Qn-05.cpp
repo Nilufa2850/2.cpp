ab3f6041032e4d907ec34add7f79444ac0ba25ab
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-02-13 20:47:02 +0530

    Create Qn-05.cpp

---
+++ Qn-05.cpp
@@ -0,0 +1,13 @@
+//WAP in c++ to calculate the volume of a cuboid.
+
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int l , b , h ;
+    cout<<"Enter length , breadth and height  : " ;
+    cin>>l>>b>>h ;
+
+    cout<<"volume is "<<l*b*h ;
+    return 0 ;
+}
