7bb6684e3145f6b453b77d095da305af1ff29a02
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-02-13 20:43:34 +0530

    Create Qn-04.cpp

---
+++ Qn-04.cpp
@@ -0,0 +1,15 @@
+//WAP in c++ to calculate the area of a cicle.
+
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int r;
+    cout<<"Enter radius : " ;
+    cin>>r ;
+
+    float area = 3.14*r*r ;
+
+    cout<<"Radius="<<r<<" Area="<<area ;
+    return 0 ;
+}
